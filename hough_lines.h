f0eddaac17d1d015e4ebd2058d9145c8b9912d0e
Shufei Chen <shufei.chen@agile-robots.com>
2022-08-24 09:02:31 +0800

    cleans up the code a bit

--- hough_lines.h
+++ hough_lines.h
@@ -52,50 +52,45 @@ namespace features
                     double min_theta = 0,
                     double max_theta = CV_PI) -> void;
 
+    /**
+     * @brief
+     * @param rho – The distance between the origin (top-left corner of an image) and the line
+     * @param theta – The angle theta formed betwe 
+     * 
+     * 
+     * 
+     * 
+     * 
+     *
+    */
+    std::vector<cv::Point> polarLine2cartPoints(double rho, double theta, double factor = 10000)
+    {
+        double x0 = rho * cos(theta);                                          // the x-coordinate of the intersection pt between the line itself and the perpendicular line where rho lands.
+        double y0 = rho * sin(theta);                                          // the y-coordinate of the intersection pt between the line itself and the perpendicular line where rho lands.
+        cv::Point p1(x0 + factor * (-sin(theta)), y0 + factor * (cos(theta))); // from the intersection point (x0, y0), go along the line leftward
+        cv::Point p2(x0 - factor * (-sin(theta)), y0 - factor * (cos(theta))); // from the intersection point (x0, y0), go along the line rightward
+        return std::vector<cv::Point>{p1, p2};
+    }
 
     /**
-     * @brief 
-     * @param image – 8-bit, single-channel binary source image. The image may be modified by the function.
      * 
-     * @param lines – Output vector of lines. Each line is represented by two cv::Point2i points.
-     * @param max_line – The line with the highest votes. If multiple lines have the same highest vote, only the first one encountered is chosen.
-     * @param d_rho – Distance resolution of the accumulator in pixels. 
-     * @param d_theta – Angle resolution of the accumulator in radians.
-     * @param threshold – Accumulator threshold parameter. Only those lines are returned that get enough votes
-     * @param min_theta – Minimum angle to check for lines. Must fall between 0 and max_theta.
-     * @param max_theta – Maximum angle to check for lines. Must fall between min_theta and CV_PI.
-     * @return void
-    */
-    auto hough_line(cv::Mat &image,
-                    std::vector<std::vector<cv::Point>> &lines,
-                    std::vector<cv::Point> &max_line,
-                    double d_rho,
-                    double d_theta,
-                    int threshold,
-                    double min_theta = 0,
-                    double max_theta = CV_PI) -> void;
-    
-        /**
-     * @brief 
-     * @param image – 8-bit, single-channel binary source image. The image may be modified by the function.
      * 
-     * @param edge_locations_vec – A STL library vector of cv::Point objects, can be the output of cv::findContours()
-     * @param lines – Output vector of lines. Each line is represented by two cv::Point2i points.
-     * @param max_line – The line with the highest votes. If multiple lines have the same highest vote, only the first one encountered is chosen.
-     * @param d_rho – Distance resolution of the accumulator in pixels. 
-     * @param d_theta – Angle resolution of the accumulator in radians.
-     * @param threshold – Accumulator threshold parameter. Only those lines are returned that get enough votes
-     * @param min_theta – Minimum angle to check for lines. Must fall between 0 and max_theta.
-     * @param max_theta – Maximum angle to check for lines. Must fall between min_theta and CV_PI.
-     * @return void
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
     */
-    auto hough_line(cv::Mat &image,
-                    std::vector<cv::Point2i> &edge_locations_vec,
-                    std::vector<std::vector<cv::Point>> &lines,
-                    std::vector<cv::Point> &max_line,
-                    double d_rho,
-                    double d_theta,
-                    int threshold,
-                    double min_theta = 0,
-                    double max_theta = CV_PI) -> void;
+    std::vector<cv::Point> reverse_roi(const cv::Point &roi_topleft, const std::vector<cv::Point> &points)
+    {
+        std::vector<cv::Point> pts = std::vector<cv::Point>(points);
+
+        std::for_each(pts.begin(), pts.end(), [&](cv::Point &pt)
+                      {
+                          pt.x += roi_topleft.x;
+                          pt.y += roi_topleft.y;
+                      });
+        return pts;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,36 +1,13 @@
-#include <yaml-cpp/yaml.h>
 #include "hough_lines.h"
+#include <yaml-cpp/yaml.h>
 
-
-static void display(const std::string &name, cv::Mat &img, double fx = 1, double fy = 1, int delay = 0)
+static void display(const std::string &name, const cv::Mat &img, double fx = 1, double fy = 1, int delay = 0)
 {
     cv::Mat img_vis;
     cv::resize(img, img_vis, cv::Size(), fx, fy);
     cv::namedWindow(name, cv::WINDOW_AUTOSIZE);
     cv::imshow(name, img_vis);
     cv::waitKey(delay);
-}
-
-static void parametric_line(cv::Mat &img, double rho, double theta, const cv::Scalar &color = cv::Scalar(0, 0, 255),
-                            int thickness = 1, int lineType = cv::LINE_AA)
-{
-    double x0 = rho * cos(theta); // the x-coordinate of the intersection pt between the line itself and the perpendicular line where rho lands.
-    double y0 = rho * sin(theta); // the y-coordinate of the intersection pt between the line itself and the perpendicular line where rho lands.
-    double factor = std::max(img.rows, img.cols);
-    cv::Point p1(x0 + factor * (-sin(theta)), y0 + factor * (cos(theta))); // from the intersection point (x0, y0), go along the line leftward 
-    cv::Point p2(x0 - factor * (-sin(theta)), y0 - factor * (cos(theta))); // from the intersection point (x0, y0), go along the line rightward
-    cv::line(img, p1, p2, color, thickness, lineType);
-}
-
-static std::vector<cv::Point> reverse_roi(cv::Point &roi_topleft, std::vector<cv::Point> &points)
-{
-    std::vector<cv::Point> pts = std::vector<cv::Point>(points);
-
-    std::for_each(pts.begin(), pts.end(), [&](cv::Point &pt) {
-        pt.x += roi_topleft.x;
-        pt.y += roi_topleft.y;
-    });
-    return pts;
 }
 
 int main(int argc, char *argv[])
@@ -54,7 +31,7 @@ int main(int argc, char *argv[])
     a. filter noise with Gaussian blurring
     b. extract white bulk with binary thresholding
 */
-    int fs = config["Gaussian_filter_size"].as<int>();
+    int fs = config["Gaussian_params"].as<int>();
     cv::GaussianBlur(image, image, cv::Size(fs, fs), 0);
     cv::threshold(image, image, config["Threshold_params"][0].as<int>(),
                   config["Threshold_params"][1].as<int>(), cv::THRESH_BINARY);
@@ -62,10 +39,32 @@ int main(int argc, char *argv[])
 
     /* 1.2 select ROI 
 */
-    cv::Rect2d roi(config["roi"][0].as<int>(), config["roi"][1].as<int>(),
-                   config["roi"][2].as<int>(), config["roi"][3].as<int>());
-    cv::Mat imCrop = image(roi);
-    display("imCrop", imCrop, 1, 0.5, 1);
+    cv::Rect roi_rect;
+    cv::Point roi_topleft; //used later for mapping the pts in roi to the original image
+
+    auto node = config["roi"];
+    if (node.IsSequence() && node[0].IsSequence())
+    {
+        std::vector<std::vector<int>> roi = node.as<std::vector<std::vector<int>>>();
+
+        std::vector<cv::Point2i> pts;
+
+        std::for_each(roi.begin(), roi.end(), [&](std::vector<int> &vec)
+                      { pts.push_back(cv::Point2i(vec[0], vec[1])); });
+
+        roi_rect = cv::boundingRect(pts);
+    }
+    else if (node.IsSequence())
+    {
+
+        std::vector<int> roi = node.as<std::vector<int>>();
+        roi_rect = cv::Rect(roi[0], roi[1], roi[2], roi[3]);
+    }
+
+    roi_topleft = roi_rect.tl();
+
+    cv::Mat imCrop = image(roi_rect);
+    display("imCrop", imCrop, 0.5, 0.5, 1);
 
     /* 2. find edges
 */
@@ -73,30 +72,20 @@ int main(int argc, char *argv[])
     cv::Mat imCrop_gray;
     cv::cvtColor(imCrop, imCrop_gray, cv::COLOR_BGR2GRAY);
     cv::Canny(imCrop_gray, edge_image, 80, 200);
-    display("edges", edge_image, 1, 0.5, 1);
+    display("edges", edge_image, 0.5, 0.5, 1);
 
     /* 3.1 find contours based on 1) thresholding image, or 2) edge image
     3.2 sort by 1) contour area, or 2) number of pixels in the contour
 */
     std::vector<std::vector<cv::Point>> contours;
-    int flag = config["Contour_params"].as<int>();
-    if (flag == 0)
-    {
-        cv::findContours(imCrop_gray, contours, cv::RETR_CCOMP, cv::CHAIN_APPROX_NONE);
-        std::sort(contours.begin(), contours.end(), [](const std::vector<cv::Point> &lhs, const std::vector<cv::Point> &rhs)
-                  { return cv::contourArea(lhs) > cv::contourArea(rhs); });
-    }
-    else if (flag == 1)
-    {
-        cv::findContours(edge_image, contours, cv::RETR_CCOMP, cv::CHAIN_APPROX_NONE);
-        std::sort(contours.begin(), contours.end(), [](const std::vector<cv::Point> &lhs, const std::vector<cv::Point> &rhs)
-                  { return lhs.size() > rhs.size(); });
-    }
-
+    cv::findContours(imCrop_gray, contours, cv::RETR_CCOMP, cv::CHAIN_APPROX_NONE);
+    std::sort(contours.begin(), contours.end(), [](const std::vector<cv::Point> &lhs, const std::vector<cv::Point> &rhs)
+              { return cv::contourArea(lhs) > cv::contourArea(rhs); });
 
     /* 3.3 remove parts of the contour at the borders of the image
 */
     int largest_x = imCrop.cols, largest_y = imCrop.rows;
+
     contours[0].erase(std::remove_if(
                           contours[0].begin(), contours[0].end(), [=](const cv::Point &pt)
                           { return pt.x > largest_x - 5 || pt.y > largest_y - 5 || pt.x < 5 || pt.y < 5; }),
@@ -104,26 +93,27 @@ int main(int argc, char *argv[])
 
     /* 4. apply hough line algorithm on both the contour and the edge image
 */
-    // std::vector<cv::Vec2d> lines_from_edges;
-    // cv::Vec2d max_line_from_edges;
-    std::vector<std::vector<cv::Point>> lines_from_edges;
-    std::vector<cv::Point> max_line_from_edges;
-    features::hough_line(edge_image, lines_from_edges, max_line_from_edges, 1, CV_PI / 180, 100, 0, CV_PI);
+    std::vector<cv::Vec2d> lines_from_edges;
+    cv::Vec2d max_line_from_edges;
+    // std::vector<std::vector<cv::Point>> lines_from_edges;
+    // std::vector<cv::Point> max_line_from_edges;
+    features::hough_line(edge_image, lines_from_edges, max_line_from_edges, 1, CV_PI / 180, config["HoughLine_params_edges"].as<int>(), 0, CV_PI);
 
-    // std::vector<cv::Vec2d> lines_from_contours;
-    // cv::Vec2d max_line_from_contours;
-    std::vector<std::vector<cv::Point>> lines_from_contours;
-    std::vector<cv::Point> max_line_from_contours;
-    features::hough_line(edge_image, contours[0], lines_from_contours, max_line_from_contours, 1, CV_PI / 180, 100, 0, CV_PI);
+    std::vector<cv::Vec2d> lines_from_contours;
+    cv::Vec2d max_line_from_contours;
+    // std::vector<std::vector<cv::Point>> lines_from_contours;
+    // std::vector<cv::Point> max_line_from_contours;
+    features::hough_line(edge_image, contours[0], lines_from_contours, max_line_from_contours, 1, CV_PI / 180, config["HoughLine_params_contours"].as<int>(), 0, CV_PI);
 
     // end timing
     auto t2 = std::chrono::high_resolution_clock::now();
     auto dt_in_ms1 = std::chrono::duration<double>(t2 - t1).count() * 1000;
     std::cout << "Total time: " << dt_in_ms1 << std::endl;
 
-//#############################################################################################################################################################
-//######################################################## drawing and display ################################################################################
-//#############################################################################################################################################################
+
+    //#############################################################################################################################################################
+    //######################################################## drawing and display ################################################################################
+    //#############################################################################################################################################################
 
     /* draw contours and contour from maximum area
     */
@@ -138,54 +128,48 @@ int main(int argc, char *argv[])
         }
     }
 
-
     // cv::drawContours(drawing, contours, 0, cv::Scalar(0, 0, 255), 1, cv::LINE_8);
     for (const auto &pt : contours[0])
         drawing.at<cv::Vec3b>(pt.y, pt.x) = cv::Vec3b(0, 0, 255);
-    display("contours", drawing, 1, 0.5, 1);
+    display("contours", drawing, 0.5, 0.5, 1);
 
     /* 4.1 draw lines above thresholds
 */
-    cv::Point roi_topleft(config["roi"][0].as<int>(), config["roi"][1].as<int>());
+    double distance_factor_btw_pts = std::max(imCrop.rows, imCrop.cols);
+
     cv::Mat from_edges;
     image.copyTo(from_edges);
+
     for (auto &params : lines_from_edges)
     {
-        // parametric_line(from_edges, params[0], params[1], cv::Scalar(255, 0, 0));
-
-        std::vector<cv::Point> pts = reverse_roi(roi_topleft, params);
-        cv::line(from_edges, pts[0], pts[1], cv::Scalar(255, 0, 0), 10);
+        std::vector<cv::Point> pts_in_roi = features::polarLine2cartPoints(params[0], params[1], distance_factor_btw_pts);
+        std::vector<cv::Point> pts_in_orig = features::reverse_roi(roi_topleft, pts_in_roi);
+        cv::line(from_edges, pts_in_orig[0], pts_in_orig[1], cv::Scalar(255, 0, 0), 10);
     }
-
-
-    cv::Mat contour_image = cv::Mat::zeros(imCrop.size(), CV_8UC3);
-    for (const auto &pt : contours[0])
-        contour_image.at<cv::Vec3b>(pt.y, pt.x) = cv::Vec3b(255, 255, 255);
-
     cv::Mat from_contours;
     image.copyTo(from_contours);
-    // imCrop.copyTo(from_contours_roi);
     for (auto &params : lines_from_contours)
     {
-        // parametric_line(from_contours, params[0], params[1], cv::Scalar(255, 0, 0));
-        std::vector<cv::Point> pts = reverse_roi(roi_topleft, params);
-        cv::line(from_contours, pts[0], pts[1], cv::Scalar(255, 0, 0), 10);
+        std::vector<cv::Point> pts_in_roi = features::polarLine2cartPoints(params[0], params[1], distance_factor_btw_pts);
+        std::vector<cv::Point> pts_in_orig = features::reverse_roi(roi_topleft, pts_in_roi);
+        cv::line(from_contours, pts_in_orig[0], pts_in_orig[1], cv::Scalar(255, 0, 0), 10);
     }
 
     /* 4.2 draw the line from highest votes
 */
-    // parametric_line(from_edges, max_line_from_edges[0], max_line_from_edges[1], cv::Scalar(0, 0, 255), 2);
-    cv::line(from_edges, reverse_roi(roi_topleft, max_line_from_edges)[0], reverse_roi(roi_topleft, max_line_from_edges)[1], cv::Scalar(0, 0, 255), 10);
-    // std::cout << "Line_from_edges of highest votes (red): { pt1: " << line_edges[0] << " pt2: " << line_edges[1] << " }" << std::endl;
+    std::vector<cv::Point> pts_from_edges = features::reverse_roi(roi_topleft, features::polarLine2cartPoints(max_line_from_edges[0], max_line_from_edges[1], distance_factor_btw_pts));
+    cv::line(from_edges, pts_from_edges[0], pts_from_edges[1], cv::Scalar(0, 0, 255), 10);
+    std::cout << "Line_from_edges of highest votes (red): { rho: " << max_line_from_edges[0] << " theta: "
+              << max_line_from_edges[1] << " }" << std::endl;
 
-    cv::line(from_contours, reverse_roi(roi_topleft, max_line_from_contours)[0], reverse_roi(roi_topleft, max_line_from_contours)[1], cv::Scalar(0, 0, 255), 10);
-    // cv::line(contour_image, max_line_from_contours[0], max_line_from_contours[1], cv::Scalar(0, 0, 255), 2);
-    // std::cout << "Line_from_contours of highest votes (red): { pt1: " << line_contours[0] << " pt2: " << line_contours[1] << " }" << std::endl;
+    std::vector<cv::Point> pts_from_contours = features::reverse_roi(roi_topleft, features::polarLine2cartPoints(max_line_from_contours[0], max_line_from_contours[1], distance_factor_btw_pts));
+    cv::line(from_contours, pts_from_contours[0], pts_from_contours[1], cv::Scalar(0, 0, 255), 10);
+    std::cout << "Line_from_contours of highest votes (red): { rho: " << max_line_from_contours[0] << " theta: "
+              << max_line_from_contours[1] << " }" << std::endl;
 
     // display images
     display("lines_from_edges", from_edges, 0.2, 0.2, 1);
     display("lines_from_contours", from_contours, 0.2, 0.2, 0);
-    // display("lines_from_contours", contour_image, 1, 0.5, 0);
 
     cv::destroyAllWindows();
 }
